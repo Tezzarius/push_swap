d8b97118882ec57aaec1a6f59630daee181308cb
bschwarz <bschwarz@student.42vienna.com>
2025-07-24 15:36:51 +0200

    fin

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bschwarz <bschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 10:03:36 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/07/24 10:35:29 by bschwarz         ###   ########.fr       */
+/*   Updated: 2025/07/24 12:53:09 by bschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int	main(int ac, char **av)
 	stack_b = (t_stack){0};
 	parsing_args(&stack_a, ac, av);
 
+	pa(&stack_a, &stack_b);
 	pa(&stack_a, &stack_b);
 	pa(&stack_a, &stack_b);
 	pa(&stack_a, &stack_b);
--- push.c
+++ push.c
@@ -6,45 +6,49 @@
 /*   By: bschwarz <bschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 14:07:21 by bschwarz          #+#    #+#             */
-/*   Updated: 2025/07/24 10:37:40 by bschwarz         ###   ########.fr       */
+/*   Updated: 2025/07/24 13:28:24 by bschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	push_to_stack(t_stack *from, t_stack *to)
+static int	push_to_stack(t_stack *from, t_stack *to)
 {
 	t_dll	*tmp;
-	
-	tmp = NULL;
+
 	if (!from->tail)
-		return ;
+		return (0);
 	tmp = from->tail;
-	if (!to->tail)
-		ft_dllinit(&to->tail, &to->head, from->tail->content);
-	else
-		ft_dlladd_beginning(&to->tail, from->tail->content);
-	if (from->tail != from->head)
+	if (tmp->next)
 	{
-		from->tail->next->prev = NULL;
-		from->tail = from->tail->next;	
+		from->tail = tmp->next;
+		tmp->next->prev = NULL;
 	}
 	else
 	{
 		from->tail = NULL;
 		from->head = NULL;
 	}
-	free(tmp);
+	if (!to->tail)
+		tmp->next = NULL;
+	else
+	{
+		tmp->next = to->tail;
+		to->tail->prev = tmp;
+	}
+	tmp->prev = NULL;
+	to->tail = tmp;
+	return (1);
 }
 
 void	pa(t_stack *stack_a, t_stack *stack_b)
 {
-	push_to_stack(stack_a, stack_b);
-	ft_printf("pa\n");
+	if (push_to_stack(stack_a, stack_b))
+		ft_printf("pa\n");
 }
 
 void	pb(t_stack *stack_a, t_stack *stack_b)
 {
-	push_to_stack(stack_b, stack_a);
-	ft_printf("pb\n");
+	if (push_to_stack(stack_b, stack_a))
+		ft_printf("pb\n");
 }
